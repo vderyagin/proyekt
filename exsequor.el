ed40c060aac8c0363bff1c1df55d016a350a3242
Victor Deryagin <vderyagin@gmail.com>
2026-01-10 01:33:37 +0200

    public domain

--- exsequor.el
+++ exsequor.el
@@ -1,7 +1,5 @@
 ;;; exsequor.el --- Run tasks in project and outside of it -*- lexical-binding: t -*-
 
-;; Copyright (C) 2023-2026 Victor Deryagin
-
 ;; Author: Victor Deryagin <vderyagin@gmail.com>
 ;; Maintainer: Victor Deryagin <vderyagin@gmail.com>
 ;; Created: 16 Dec 2023
@@ -11,20 +9,8 @@
 
 ;; This file is not part of GNU Emacs.
 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2, or (at
-;; your option) any later version.
-
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;; This is free and unencumbered software released into the public domain.
+;; For more information, please refer to <http://unlicense.org/>
 
 ;;; Commentary:
 
